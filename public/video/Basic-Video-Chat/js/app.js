ee7efeb541ff4b48bb141fc2b4346b0fda28b6db
Richard Sabbarton <richard.sabbarton@gmail.com>
2026-09-10 10:00:47 +0100

    Refactor app.js and subscribe-only.js for improved readability and consistency

--- public/video/Basic-Video-Chat/js/app.js
+++ public/video/Basic-Video-Chat/js/app.js
@@ -1,27 +1,26 @@
+let publisher;
 
-let publisher
+let apiKey;
+let sessionId;
+let token;
 
-let apiKey
-let sessionId
-let token
+let timestamps = {};
+timestamps.load = performance.now();
 
-let timestamps = {}
-timestamps.load = performance.now()
-
-let roomName = new URLSearchParams(window.location.search).get('roomName')
-let urlApiKey = new URLSearchParams(window.location.search).get('apiKey')
-if(!urlApiKey) urlApiKey = '47807831'
-console.log('Using Room: ' + roomName)
-console.log('Using apiKey: ' + urlApiKey)
-timestamps.getSessionCredentials = performance.now()
-getSessionCredentials(urlApiKey, roomName)
-.then(json=>{
-  timestamps.getSessionCredentialsTook = performance.now() - timestamps.getSessionCredentials
-  apiKey = json.apiKey
-  sessionId = json.sessionId
-  token = json.token
-  initializeSession()
-})
+let roomName = new URLSearchParams(window.location.search).get("roomName");
+let urlApiKey = new URLSearchParams(window.location.search).get("apiKey");
+if (!urlApiKey) urlApiKey = "47807831";
+console.log("Using Room: " + roomName);
+console.log("Using apiKey: " + urlApiKey);
+timestamps.getSessionCredentials = performance.now();
+getSessionCredentials(urlApiKey, roomName).then((json) => {
+  timestamps.getSessionCredentialsTook =
+    performance.now() - timestamps.getSessionCredentials;
+  apiKey = json.apiKey;
+  sessionId = json.sessionId;
+  token = json.token;
+  initializeSession();
+});
 
 function handleError(error) {
   if (error) {
@@ -29,67 +28,74 @@ function handleError(error) {
   }
 }
 
-
-
-
 function initializeSession() {
+  timestamps.init = performance.now();
 
-  timestamps.init = performance.now()
+  const session = OT.initSession(apiKey, sessionId, {
+    sessionMigration: true,
+  });
 
-  const session = OT.initSession(apiKey, sessionId);
-
-  timestamps.initSessionTook = performance.now() - timestamps.init
+  timestamps.initSessionTook = performance.now() - timestamps.init;
   // Subscribe to a newly created stream
-  session.on('streamCreated', (event) => {
-    console.log('Stream Created:')
+  session.on("streamCreated", (event) => {
+    console.log("Stream Created:");
     const subscriberOptions = {
-      insertMode: 'append',
-      width: '100%',
-      height: '100%',
+      insertMode: "append",
+      width: "100%",
+      height: "100%",
       publishVideo: false,
-      publishAudio: false
+      publishAudio: false,
     };
-    timestamps.subscribe = performance.now()
-    session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+    timestamps.subscribe = performance.now();
+    session.subscribe(
+      event.stream,
+      "subscriber",
+      subscriberOptions,
+      handleError,
+    );
   });
 
-  session.on('sessionDisconnected', (event) => {
-    console.log('You were disconnected from the session.', event.reason);
+  session.on("sessionDisconnected", (event) => {
+    console.log("You were disconnected from the session.", event.reason);
   });
-  
-  session.on("streamDestroyed", (event)=>{
+
+  session.on("streamDestroyed", (event) => {
     if (event.reason === "clientDisconnected") {
-    console.log("Call disconnected by the user");
-    }  
+      console.log("Call disconnected by the user");
+    }
   });
 
-  session.on("connectionCreated", (event)=>{ console.log(event.type, event.connection.id)})
-  session.on("connectionDestroyed", (event)=>{ console.log(event.type, event.connection.id)})
-  
+  session.on("connectionCreated", (event) => {
+    console.log(event.type, event.connection.id);
+  });
+  session.on("connectionDestroyed", (event) => {
+    console.log(event.type, event.connection.id);
+  });
+
   // initialize the publisher
   const publisherOptions = {
-    insertMode: 'append',
-    width: '100%',
-    height: '100%',
-    resolution: '1280x720'
+    insertMode: "append",
+    width: "100%",
+    height: "100%",
+    resolution: "1280x720",
   };
-  timestamps.createPublisher = performance.now()
-  publisher = OT.initPublisher('publisher', publisherOptions, handleError);
-  timestamps.createPublisherTook = performance.now() - timestamps.createPublisher
+  timestamps.createPublisher = performance.now();
+  publisher = OT.initPublisher("publisher", publisherOptions, handleError);
+  timestamps.createPublisherTook =
+    performance.now() - timestamps.createPublisher;
 
   // Connect to the session
-  timestamps.sessionConnect = performance.now()
+  timestamps.sessionConnect = performance.now();
   session.connect(token, (error) => {
-    timestamps.sessionConnectTook = performance.now() - timestamps.sessionConnect
-    timestamps.sessionConnected = performance.now()
+    timestamps.sessionConnectTook =
+      performance.now() - timestamps.sessionConnect;
+    timestamps.sessionConnected = performance.now();
     if (error) {
       handleError(error);
     } else {
       // If the connection is successful, publish the publisher to the session
-      timestamps.publish = performance.now()
+      timestamps.publish = performance.now();
       session.publish(publisher, handleError);
     }
   });
 }
-
-
--- public/video/Subscribe-Only/js/app.js
+++ public/video/Subscribe-Only/js/app.js
@@ -1,23 +1,20 @@
+let publisher;
+let subscriber;
+let apiKey;
+let sessionId;
+let token;
 
-let publisher
-let subscriber
-let apiKey
-let sessionId
-let token
-
-
-let roomName = new URLSearchParams(window.location.search).get('roomName')
-let urlApiKey = new URLSearchParams(window.location.search).get('apiKey')
-if(!urlApiKey) urlApiKey = '47807831'
-console.log('Using Room: ' + roomName)
-console.log('Using apiKey: ' + urlApiKey)
-getSessionCredentials(urlApiKey, roomName)
-.then(json=>{
-  apiKey = json.apiKey
-  sessionId = json.sessionId
-  token = json.token
-  initializeSession()
-})
+let roomName = new URLSearchParams(window.location.search).get("roomName");
+let urlApiKey = new URLSearchParams(window.location.search).get("apiKey");
+if (!urlApiKey) urlApiKey = "47807831";
+console.log("Using Room: " + roomName);
+console.log("Using apiKey: " + urlApiKey);
+getSessionCredentials(urlApiKey, roomName).then((json) => {
+  apiKey = json.apiKey;
+  sessionId = json.sessionId;
+  token = json.token;
+  initializeSession();
+});
 
 function handleError(error) {
   if (error) {
@@ -25,44 +22,49 @@ function handleError(error) {
   }
 }
 
-
-
-
 function initializeSession() {
-
-  console.log('init session')
+  console.log("init session");
   const session = OT.initSession(apiKey, sessionId);
 
   // Subscribe to a newly created stream
-  session.on('streamCreated', (event) => {
+  session.on("streamCreated", (event) => {
     const subscriberOptions = {
-      insertMode: 'append',
-      width: '100%',
-      height: '100%'
+      insertMode: "append",
+      width: "100%",
+      height: "100%",
     };
-    console.log('subscribing')
-    subscriber = session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+    console.log("subscribing");
+    subscriber = session.subscribe(
+      event.stream,
+      null,
+      subscriberOptions,
+      handleError,
+    );
   });
 
-  session.on('sessionDisconnected', (event) => {
-    console.log('You were disconnected from the session.', event.reason);
+  session.on("sessionDisconnected", (event) => {
+    console.log("You were disconnected from the session.", event.reason);
   });
-  
-  session.on("streamDestroyed", (event)=>{
+
+  session.on("streamDestroyed", (event) => {
     if (event.reason === "clientDisconnected") {
-    console.log("Call disconnected by the user");
-    }  
+      console.log("Call disconnected by the user");
+    }
   });
 
-  session.on("connectionCreated", (event)=>{ console.log(event.type, event.connection.id)})
-  session.on("connectionDestroyed", (event)=>{ console.log(event.type, event.connection.id)})
-  
+  session.on("connectionCreated", (event) => {
+    console.log(event.type, event.connection.id);
+  });
+  session.on("connectionDestroyed", (event) => {
+    console.log(event.type, event.connection.id);
+  });
+
   session.connect(token, (error) => {
     if (error) {
       handleError(error);
     } else {
       // If the connection is successful, publish the publisher to the session
-      console.log('connected')
+      console.log("connected");
     }
-  })
+  });
 }
